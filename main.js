eb162e9a683b53735cc8c7755524823bef696499
Miguel Martinez de Aguirre <miguel@aguirredevelopment.co.uk>
2015-05-23 16:38:48 +0100

    Use namespace, create "anchor" circles on click.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-var stage;
+var lowpoly = lowpoly || {};
+lowpoly.anchorFor = {};
 
-function init()
+lowpoly.init = function()
 {
 	var width = window.innerWidth * 0.9;
 	var height = window.innerHeight * 0.9;
 
-	stage = new Konva.Stage({
+	lowpoly.stage = new Konva.Stage({
 		container: 'container',
 		width: width,
 		height: height
@@ -14,5 +15,22 @@ function init()
 	var layer = new Konva.Layer();
 	layer.name = "main"
 
-	stage.add(layer);
+	lowpoly.stage.add(layer);
+
+	lowpoly.stage.on('contentClick', lowpoly.stageClick);
+}
+
+lowpoly.stageClick = function(event)
+{
+	var anchor = new Konva.Circle({
+		radius: 5,
+		x: event.evt.x,
+		y: event.evt.y,
+		fill: 'white',
+		stroke: 'black'
+	});
+
+	var layer = lowpoly.stage.find("Layer")[0];
+	layer.add(anchor);
+	lowpoly.stage.add(layer);
 }
